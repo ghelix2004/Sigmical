6b83fd9426fb1331b234b842196a4c76b582d1a4
Tran Cong Toan <thanhnoobs@gmail.com>
2021-12-12 10:02:04 +0700

    sigmical-upload

---
+++ public/examples/EQ/reaction.js
@@ -0,0 +1,40 @@
+var rct = {
+    "C + O2":{
+       "Form":"C + O₂ → CO₂",
+       "Reaction":"c+o2+co2.svg",
+       "ig1":"",
+       "ig2":"https://www.youtube.com/embed/BNf-Y9w-Hm4",
+       "Info":{
+          "CT":"CO₂",
+          "DS":"-78,46 °C",
+          "IUP":"Cacbon Dioxit",
+          "M":"44,01 g/mol",
+          "PL":"Oxit Axit",
+          "HT":"Nước (H₂O)",
+          "DE":"Carbon dioxide hay cacbonic oxit là một hợp chất ở điều kiện bình thường có dạng khí trong khí quyển Trái Đất, bao gồm một nguyên tử carbon và hai nguyên tử oxy. Là một hợp chất hóa học được biết đến rộng rãi, nó thường xuyên được gọi theo công thức hóa học là CO₂. Trong dạng rắn, nó được gọi là băng khô hoặc đá khô.",
+          "link":"",
+          "i1":"Phản ứng đốt than trong không khí thu được khí CO2.",
+          "NB":"Phản ứng nhận biết CO₂ sục qua khí Ca(OH)₂ thu được kết tủa trắng CaCO₃<br/><img src='./equation/co2+caoh2.svg'/>"
+       }
+    },
+    "CO2 + Ca(OH)2":{
+      "Form":"CO₂ + Ca(OH)₂ → CaCO₃↓ + H₂O",
+      "Reaction":"co2+caoh2.svg",
+      "ig1":"",
+      "ig2":"https://www.youtube.com/embed/jmQXLE3kIPk",
+      "Info":{
+         "CT":"CaCO₃↓",
+         "IUP":"Canxi Cacbonat",
+         "DS":"825 °C",
+         "M":"100,0869 g/mol",
+         "PL":"Khoáng vật Cacbonat",
+         "HT":"HCl (Axit Clohidric)",
+         "DE":"Canxi cacbonat là một hợp chất hóa học với công thức hóa học là CaCO₃. Đây là một chất thường được sử dụng trong y tế như một chất bổ sung canxi cho người bị loãng xương, cung cấp canxi cho cơ thể hay một chất khử chua.",
+         "link":"",
+         "i1":"Phản ứng sục khí CO₂ qua dung dịch Ca(OH)₂ xuất hiện kết tủa trắng CaCO₃↓",
+         "NB":"Phản ứng hòa tan Ca(OH)₂ trong dung dịch HCl giải phóng khí CO₂<br/><img src='./equation/caco3+2hcl.svg'/>"
+      }
+   },
+   
+};
+var eq=["C + O₂ → CO₂","CO₂ + Ca(OH)₂ → CaCO₃↓ + H₂O"];
---
+++ public/examples/connect.js
@@ -0,0 +1,6 @@
+document.addEventListener('start', (e)=>{
+    
+});
+document.getElementById("button1").onclick = function(){
+    alert()
+   }
